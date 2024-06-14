0d5491a3a169a1439ff716cc583f46460e83dcf0
valentine <valentine.madu@zedcrest.com>
2024-06-14 04:16:19 +0100

    wip

---
+++ golang-linked-list/simpleLL.go
@@ -0,0 +1,24 @@
+package main
+
+type node struct {
+	number int
+	next   *node
+}
+
+type linkedList struct {
+	head *node
+	len  int
+}
+
+func (l *linkedList) insert(num int) {
+	newNode := new(node)
+	newNode.number = num
+	newNode.next = nil
+
+	if l.head == nil {
+		l.head = newNode
+	} else {
+		iterator := l.head
+	}
+
+}
