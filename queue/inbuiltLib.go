b11a50b70b6cf9dd09341d72718eb7da25692eb3
valentine <valentine.madu@zedcrest.com>
2024-06-22 12:31:33 +0100

    wip

---
+++ queue/inbuiltLib.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	queue := list.New()
+	queue.PushFront("Everybody")
+	queue.PushFront("Loves")
+	queue.PushFront("Val")
+
+	for {
+		front := queue.Front()
+		if front != nil {
+			fmt.Println(front.Value)
+			queue.Remove(front)
+		} else {
+			fmt.Println("Queue is now empty")
+			return
+		}
+	}
+	return
+}
