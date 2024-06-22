ed9ecfd4a935353e2498d4c143445f196039832a
valentine <valentine.madu@zedcrest.com>
2024-06-22 17:41:09 +0100

    wip

---
+++ sorting/merge.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func merge(left []int, right []int) []int {
+	final := []int{}
+	i := 0
+	j := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			final = append(final, left[i])
+		} else {
+			final = append(final, right[j])
+		}
+	}
+	for ; i < len(left); i++ {
+		final = append(final, left[i])
+	}
+	for ; j < len(right); j++ {
+		final = append(final, right[j])
+	}
+	return final
+}
+
+func mergeSort(items []int) []int {
+	if len(items) < 2 {
+		return items
+	}
+	first := mergeSort(items[:len(items)/2])
+	second := mergeSort(items[len(items)/2:])
+	return merge(first, second)
+}
+
+func main() {
+	unsorted := []int{10, 6, 2, 1, 5, 8, 3, 4, 7, 9}
+	sorted := mergeSort(unsorted)
+
+	fmt.Println(sorted)
+}
